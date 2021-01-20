378eb32f6c2fdc2e13720b96c5bd772257d0bb66
kabir-webDev <kabir.webdev@gmail.com>
2021-01-20 16:04:52 +0600

    Posting to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,31 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+// import "firebase/auth";
 
-const firebaseConfig = {
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBn_aCAKcgpMqYLgRAV9qwUM9VdgzUdXfo",
+//   authDomain: "firbase-learn-360.firebaseapp.com",
+//   projectId: "firbase-learn-360",
+//   storageBucket: "firbase-learn-360.appspot.com",
+//   messagingSenderId: "196987928754",
+//   appId: "1:196987928754:web:d41acbd8444bbaa47d49ab",
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase;
+
+import firebase from "firebase";
+
+var firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBn_aCAKcgpMqYLgRAV9qwUM9VdgzUdXfo",
   authDomain: "firbase-learn-360.firebaseapp.com",
   projectId: "firbase-learn-360",
   storageBucket: "firbase-learn-360.appspot.com",
   messagingSenderId: "196987928754",
   appId: "1:196987928754:web:d41acbd8444bbaa47d49ab",
-};
+});
 
-firebase.initializeApp(firebaseConfig);
+var db = firebaseApp.firestore();
 
-export default firebase;
+export { db };
